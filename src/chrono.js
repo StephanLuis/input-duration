1d132dc4c21c1a5430e9eee3e6bf4d265c4d656a
StephanLuis <stephan@learnsense.org>
2022-02-10 10:49:58 +0000

    merge module_chrono (good parts) with chrono.js

--- src/chrono.js
+++ src/chrono.js
@@ -18,7 +18,8 @@ class ChronlyHMS {
     this.addLeftRightToggle();
     this.addNumericInput();
     this.addClicksToActivate();
-    this.updateDivPrototype()
+    this.nonNumericBugInHTML();
+    this.updateDivPrototype();
 
   }
 
@@ -42,19 +43,39 @@ class ChronlyHMS {
       spanIn.classList.add('timeCase');
 
       var template = document.createElement('template');
+     
+      //old
+     
+      // template.innerHTML = `
+      //     <input type="number" class="D2 ts_digit" name="startHours" id="sH" data-tp="1" min="-1" max="100" value="00" ><span class="bds">:</span>
+      //     <input type="number" class="D2 ts_digit" name="startMinutes" id="sM" data-tp="2" min="-1" max="60" value="00"><span class="bds">:</span>
+      //     <input type="number" class="D2 ts_digit" name="startSeconds" id="sS" data-tp="3" min="-1" max="60" value="00"><span class="bds">.</span>
+      //     <input type="number" class="D3 ts_digit" name="startMilliSecs" id="sMS" data-tp="4" min="-10" max="1010" step="10" value="000">
+      //     <div id="svgContainer">
+      //       <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+      //         focusable="false" data-prefix="fal" data-icon="stopwatch" class="svg-inline--fa fa-stopwatch fa-w-14"
+      //         role="img" viewBox="0 0 448 512">
+      //         <path xmlns="http://www.w3.org/2000/svg" fill="currentColor" d="M393.9 184l22.6-22.6c4.7-4.7 4.7-12.3 0-17l-17-17c-4.7-4.7-12.3-4.7-17 0l-20.7 20.7c-31.1-27.5-70.4-45.9-113.8-50.8V48h28c6.6 0 12-5.4 12-12V12c0-6.6-5.4-12-12-12H172c-6.6 0-12 5.4-12 12v24c0 6.6 5.4 12 12 12h28v49.4C96.4 109.3 16 197.2 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-44.7-14.1-86.1-38.1-120zM224 464c-88.4 0-160-71.6-160-160s71.6-160 160-160 160 71.6 160 160-71.6 160-160 160zm12-112h-24c-6.6 0-12-5.4-12-12V204c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v136c0 6.6-5.4 12-12 12z"/>
+      //       </svg>
+      //     </div>
+      // `
+
       template.innerHTML = `
-          <input type="number" class="D2 ts_digit" name="startHours" id="sH" data-tp="1" min="-1" max="100" value="00" ><span class="bds">:</span>
-          <input type="number" class="D2 ts_digit" name="startMinutes" id="sM" data-tp="2" min="-1" max="60" value="00"><span class="bds">:</span>
-          <input type="number" class="D2 ts_digit" name="startSeconds" id="sS" data-tp="3" min="-1" max="60" value="00"><span class="bds">.</span>
-          <input type="number" class="D3 ts_digit" name="startMilliSecs" id="sMS" data-tp="4" min="-10" max="1010" step="10" value="000">
-          <div id="svgContainer">
-            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-              focusable="false" data-prefix="fal" data-icon="stopwatch" class="svg-inline--fa fa-stopwatch fa-w-14"
-              role="img" viewBox="0 0 448 512">
-              <path xmlns="http://www.w3.org/2000/svg" fill="currentColor" d="M393.9 184l22.6-22.6c4.7-4.7 4.7-12.3 0-17l-17-17c-4.7-4.7-12.3-4.7-17 0l-20.7 20.7c-31.1-27.5-70.4-45.9-113.8-50.8V48h28c6.6 0 12-5.4 12-12V12c0-6.6-5.4-12-12-12H172c-6.6 0-12 5.4-12 12v24c0 6.6 5.4 12 12 12h28v49.4C96.4 109.3 16 197.2 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-44.7-14.1-86.1-38.1-120zM224 464c-88.4 0-160-71.6-160-160s71.6-160 160-160 160 71.6 160 160-71.6 160-160 160zm12-112h-24c-6.6 0-12-5.4-12-12V204c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v136c0 6.6-5.4 12-12 12z"/>
-            </svg>
-          </div>
-      `
+      <input type="number" class="sH D2 ts_digit" name="startHours" data-tp="1" min="-1" max="100" value="00" >
+      <span class="bds">:</span>
+      <input type="number" class="sM D2 ts_digit" name="startMinutes" data-tp="2" min="-1" max="60" value="00">
+      <span class="bds">:</span>
+      <input type="number" class="sS D2 ts_digit" name="startSeconds" data-tp="3" min="-1" max="60" value="00">
+      <span class="bds">.</span>
+      <input type="number" class="sMS D3 ts_digit" name="startMilliSecs" data-tp="4" min="-10" max="1010" step="10" value="000">
+      <div id="svgContainer">
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+          focusable="false" data-prefix="fal" data-icon="stopwatch" class="svg-inline--fa fa-stopwatch fa-w-14"
+          role="img" viewBox="0 0 448 512">
+          <path xmlns="http://www.w3.org/2000/svg" fill="currentColor" d="M393.9 184l22.6-22.6c4.7-4.7 4.7-12.3 0-17l-17-17c-4.7-4.7-12.3-4.7-17 0l-20.7 20.7c-31.1-27.5-70.4-45.9-113.8-50.8V48h28c6.6 0 12-5.4 12-12V12c0-6.6-5.4-12-12-12H172c-6.6 0-12 5.4-12 12v24c0 6.6 5.4 12 12 12h28v49.4C96.4 109.3 16 197.2 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-44.7-14.1-86.1-38.1-120zM224 464c-88.4 0-160-71.6-160-160s71.6-160 160-160 160 71.6 160 160-71.6 160-160 160zm12-112h-24c-6.6 0-12-5.4-12-12V204c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v136c0 6.6-5.4 12-12 12z"/>
+        </svg>
+      </div>
+  `
 
       spanIn.appendChild(template.content)
       console.log(el);
@@ -76,65 +97,136 @@ class ChronlyHMS {
     }));
 
     // input for milliseconds
+    // old
 
-    document.querySelector("#sMS").addEventListener('change', function () {
+    // document.querySelector("#sMS").addEventListener('change', function () {
+    //   if (!isNaN(this.value) && this.value.length === 2) {
+    //     this.value = '0' + this.value;
+    //   }
+
+    // });
+
+    // milliseconds
+    document.querySelectorAll("input.sMS").forEach(i => i.addEventListener('change', function () {
+    
       if (!isNaN(this.value) && this.value.length === 2) {
         this.value = '0' + this.value;
       }
 
-    });
+    }));
+
   }
 
 
   // numeric value looping
+  // old
+
+  // addNumberLooping() {
+  //   // Numeric Value looping (eventually this can be 'parameterised' refactored)
+  //   // ex. 99 uparrow to 0 hours, 59 uparrow to 0 minutes and seconds, 990 uparrow to 010, downarrow 000 milliseconds
+
+
+  //   document.querySelector("#sH")
+  //     .addEventListener('change', function (event) {
+  //       if (document.querySelector("#sH").value == 100) {
+  //         document.querySelector("#sH").value = '00';
+  //       }
+  //       if (document.querySelector("#sH").value == -1) {
+  //         document.querySelector("#sH").value = 99;
+  //       }
+  //     });
+
+  //   document.querySelector("#sM")
+  //     .addEventListener('change', function (event) {
+  //       if (document.querySelector("#sM").value == 60) {
+  //         document.querySelector("#sM").value = '00';
+  //       }
+  //       if (document.querySelector("#sM").value == -1) {
+  //         document.querySelector("#sM").value = 59;
+  //       }
+  //     });
+
+  //   document.querySelector("#sS")
+  //     .addEventListener('change', function (event) {
+  //       if (document.querySelector("#sS").value == 60) {
+  //         document.querySelector("#sS").value = '00';
+  //       }
+  //       if (document.querySelector("#sS").value == -1) {
+  //         document.querySelector("#sS").value = 59;
+  //       }
+  //     });
+
+  //   document.querySelector("#sMS")
+  //     .addEventListener('change', function (event) {
+  //       if (document.querySelector("#sMS").value == 1000) {
+  //         document.querySelector("#sMS").value = "010";
+  //       }
+  //       if (document.querySelector("#sMS").value == -10) {
+  //         document.querySelector("#sMS").value = 990;
+  //       }
+  //       if (document.querySelector("#sMS").value == 0) {
+  //       }
+  //     });
+
+  // }
+
 
   addNumberLooping() {
     // Numeric Value looping (eventually this can be 'parameterised' refactored)
     // ex. 99 uparrow to 0 hours, 59 uparrow to 0 minutes and seconds, 990 uparrow to 010, downarrow 000 milliseconds
 
+    document.querySelectorAll("input.sH").forEach(i =>
+      i.addEventListener('change', function (event) {
 
-    document.querySelector("#sH")
-      .addEventListener('change', function (event) {
-        if (document.querySelector("#sH").value == 100) {
-          document.querySelector("#sH").value = '00';
+        if (i.value == 100) {
+          i.value = '00';
         }
-        if (document.querySelector("#sH").value == -1) {
-          document.querySelector("#sH").value = 99;
+        if (i.value == -1) {
+          i.value = 99;
         }
-      });
 
-    document.querySelector("#sM")
-      .addEventListener('change', function (event) {
-        if (document.querySelector("#sM").value == 60) {
-          document.querySelector("#sM").value = '00';
-        }
-        if (document.querySelector("#sM").value == -1) {
-          document.querySelector("#sM").value = 59;
-        }
-      });
+      }));
 
-    document.querySelector("#sS")
-      .addEventListener('change', function (event) {
-        if (document.querySelector("#sS").value == 60) {
-          document.querySelector("#sS").value = '00';
-        }
-        if (document.querySelector("#sS").value == -1) {
-          document.querySelector("#sS").value = 59;
-        }
-      });
+    document.querySelectorAll("input.sM").forEach(i =>
+      i.addEventListener('change', function (event) {
 
-    document.querySelector("#sMS")
-      .addEventListener('change', function (event) {
-        if (document.querySelector("#sMS").value == 1000) {
-          document.querySelector("#sMS").value = "010";
+        if (i.value == 60) {
+          i.value = '00';
         }
-        if (document.querySelector("#sMS").value == -10) {
-          document.querySelector("#sMS").value = 990;
+        if (i.value == -1) {
+          i.value = 59;
         }
-        if (document.querySelector("#sMS").value == 0) {
-        }
-      });
 
+      }));
+
+    document.querySelectorAll("input.sS").forEach(i =>
+      i.addEventListener('change', function (event) {
+
+        if (i.value == 60) {
+          i.value = '00';
+        }
+
+        if (i.value == -1) {
+          i.value = 59;
+        }
+
+      }));
+
+    document.querySelectorAll("input.sMS").forEach(i =>
+      i.addEventListener('change', function (event) {
+
+        if (i.value == 1000) {
+          i.value = "010";
+        }
+
+        if (i.value == -10) {
+          i.value = 990;
+        }
+
+        if (i.value == 0) {
+        }
+
+      }));
   }
 
 
@@ -226,79 +318,151 @@ class ChronlyHMS {
     }
 
     // three types of boxes: hours, minutes and seconds (are the same), or milliseconds
+    // old ID based
 
-    setInputFilter(document.getElementById("sH"), function (value) {
+    //   setInputFilter(document.getElementById("sH"), function (value) {
 
+
+    //     var newV = /^([0-9]?|[0-9][0-9]?)$/.test(value);
+
+    //     console.log("value: " + value);
+
+    //     if (value.length === 2) {
+    //       document.getElementById("sM").focus();
+    //       document.getElementById("sM").select();
+
+    //     }
+
+    //     return newV;
+
+    //   });
+
+
+    //   setInputFilter(document.getElementById("sM"), function (value) {
+
+
+    //     var newV = /^([0-9]?|[0-5][0-9]?)$/.test(value);
+
+    //     console.log("value: " + value);
+
+    //     if (value > 5 || value.length === 2) {
+    //       document.getElementById("sS").focus();
+    //       document.getElementById("sS").select();
+    //     }
+
+    //     return newV;
+
+    //   });
+
+
+    //   setInputFilter(document.getElementById("sS"), function (value) {
+
+
+    //     var newV = /^([0-9]?|[0-5][0-9]?)$/.test(value);
+
+    //     console.log("value: " + value);
+
+    //     if (value > 5 || value.length === 2) {
+    //       document.getElementById("sMS").focus();
+    //       document.getElementById("sMS").select();
+    //     }
+
+    //     return newV;
+
+    //   });
+
+
+
+    //   setInputFilter(document.getElementById("sMS"), function (value) {
+
+
+    //     var newV = /^([0-9]?|[0-9][0-9]?|[0-9][0-9][0-9]?)$/.test(value);
+
+    //     console.log("value: " + value);
+
+    //     if (value.length === 3) {
+    //       document.getElementById("sMS").blur();
+    //     }
+
+    //     return newV;
+
+    //   });
+
+    // }
+
+
+
+    // three boxes, hours or minutes and seconds (are the same) but milliseconds is different
+
+    // sH
+    document.querySelectorAll('input.sH').forEach(i => setInputFilter(i, function (value) {
 
       var newV = /^([0-9]?|[0-9][0-9]?)$/.test(value);
 
-      console.log("value: " + value);
-
       if (value.length === 2) {
-        document.getElementById("sM").focus();
-        document.getElementById("sM").select();
+
+        i.parentNode.querySelector('input.sM').focus();
+        i.parentNode.querySelector('input.sM').select();
 
       }
 
       return newV;
 
-    });
+    }));
 
-
-    setInputFilter(document.getElementById("sM"), function (value) {
+    document.querySelectorAll('input.sM').forEach(i => setInputFilter(i, function (value) {
 
 
       var newV = /^([0-9]?|[0-5][0-9]?)$/.test(value);
 
-      console.log("value: " + value);
-
       if (value > 5 || value.length === 2) {
-        document.getElementById("sS").focus();
-        document.getElementById("sS").select();
+
+        i.parentNode.querySelector('input.sS').focus();
+        i.parentNode.querySelector('input.sS').select();
+
       }
 
       return newV;
 
-    });
+    }));
 
-
-    setInputFilter(document.getElementById("sS"), function (value) {
-
+    document.querySelectorAll('input.sS').forEach(i => setInputFilter(i, function (value) {
 
       var newV = /^([0-9]?|[0-5][0-9]?)$/.test(value);
 
-      console.log("value: " + value);
-
       if (value > 5 || value.length === 2) {
-        document.getElementById("sMS").focus();
-        document.getElementById("sMS").select();
+
+        i.parentNode.querySelector('input.sMS').focus();
+        i.parentNode.querySelector('input.sMS').select();
+
       }
 
       return newV;
 
-    });
+    }));
 
 
-
-    setInputFilter(document.getElementById("sMS"), function (value) {
-
+    document.querySelectorAll('input.sMS').forEach(i => setInputFilter(i, function (value) {
 
       var newV = /^([0-9]?|[0-9][0-9]?|[0-9][0-9][0-9]?)$/.test(value);
-
-      console.log("value: " + value);
+      
 
       if (value.length === 3) {
-        document.getElementById("sMS").blur();
+
+        i.parentNode.querySelector('input.sMS').blur();
+
       }
 
       return newV;
 
-    });
+    }));
 
   }
 
 
+
   // click to activate input of values by the keyboard
+  // old
 
   addClicksToActivate() {
 
@@ -322,8 +486,35 @@ class ChronlyHMS {
     // document.querySelector("#sMS").addEventListener("click", function () { this.select(); });
 
     document.querySelectorAll('input[name="startMilliSecs"]').forEach(
-      item => item.addEventListener("click", function () { this.select();})
+      item => item.addEventListener("click", function () { this.select(); })
     )
+
+  }
+
+  // click to activate input of values
+  // addClicksToActivate() {
+
+  //   document.querySelectorAll("input.sH").forEach(sh => sh.addEventListener("click", function () { this.select(); }));
+  //   document.querySelectorAll("input.sM").forEach(sm => sm.addEventListener("click", function () { this.select(); }));
+  //   document.querySelectorAll("input.sS").forEach(ss => ss.addEventListener("click", function () { this.select(); }));
+  //   document.querySelectorAll("input.sMS").forEach(sms => sms.addEventListener("click", function () { this.select(); }));
+
+  // }
+
+  nonNumericBugInHTML() {
+
+    document.querySelectorAll(".timeCase input").forEach(item =>
+
+      item.addEventListener('keypress', function (e) {
+
+        e = e || window.event;
+        var charCode = (typeof e.which == "undefined") ? e.keyCode : e.which;
+        var charStr = String.fromCharCode(charCode);
+
+        if (!charStr.match(/^[0-9]+$/))
+          e.preventDefault();
+
+      }));
 
   }
 
@@ -340,7 +531,7 @@ class ChronlyHMS {
         return this.querySelector('input[name="startHours"]').value +
           ':' + this.querySelector('input[name="startMinutes"]').value +
           ':' + this.querySelector('input[name="startSeconds"]').value +
-          ':' + this.querySelector('input[name="startMilliSecs"]').value;
+          '.' + this.querySelector('input[name="startMilliSecs"]').value;
 
       }
       else {
