da47d3fb3b6b472d594592f1be651026f2b07b6e
StephanLuis <stephan@learnsense.org>
2022-02-10 12:41:52 +0000

    update to arrowkeyinput, removed module_chrono.js

--- src/chrono.js
+++ src/chrono.js
@@ -15,7 +15,7 @@ class ChronlyHMS {
     // set up event listeners
     this.add0s();
     this.addNumberLooping();
-    this.addLeftRightToggle();
+    this.addArrowKeyInput();
     this.addNumericInput();
     this.addClicksToActivate();
     this.nonNumericBugInHTML();
@@ -43,22 +43,6 @@ class ChronlyHMS {
       spanIn.classList.add('timeCase');
 
       var template = document.createElement('template');
-     
-      //old
-     
-      // template.innerHTML = `
-      //     <input type="number" class="D2 ts_digit" name="startHours" id="sH" data-tp="1" min="-1" max="100" value="00" ><span class="bds">:</span>
-      //     <input type="number" class="D2 ts_digit" name="startMinutes" id="sM" data-tp="2" min="-1" max="60" value="00"><span class="bds">:</span>
-      //     <input type="number" class="D2 ts_digit" name="startSeconds" id="sS" data-tp="3" min="-1" max="60" value="00"><span class="bds">.</span>
-      //     <input type="number" class="D3 ts_digit" name="startMilliSecs" id="sMS" data-tp="4" min="-10" max="1010" step="10" value="000">
-      //     <div id="svgContainer">
-      //       <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-      //         focusable="false" data-prefix="fal" data-icon="stopwatch" class="svg-inline--fa fa-stopwatch fa-w-14"
-      //         role="img" viewBox="0 0 448 512">
-      //         <path xmlns="http://www.w3.org/2000/svg" fill="currentColor" d="M393.9 184l22.6-22.6c4.7-4.7 4.7-12.3 0-17l-17-17c-4.7-4.7-12.3-4.7-17 0l-20.7 20.7c-31.1-27.5-70.4-45.9-113.8-50.8V48h28c6.6 0 12-5.4 12-12V12c0-6.6-5.4-12-12-12H172c-6.6 0-12 5.4-12 12v24c0 6.6 5.4 12 12 12h28v49.4C96.4 109.3 16 197.2 16 304c0 114.9 93.1 208 208 208s208-93.1 208-208c0-44.7-14.1-86.1-38.1-120zM224 464c-88.4 0-160-71.6-160-160s71.6-160 160-160 160 71.6 160 160-71.6 160-160 160zm12-112h-24c-6.6 0-12-5.4-12-12V204c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v136c0 6.6-5.4 12-12 12z"/>
-      //       </svg>
-      //     </div>
-      // `
 
       template.innerHTML = `
       <input type="number" class="sH D2 ts_digit" name="startHours" data-tp="1" min="-1" max="100" value="00" >
@@ -96,19 +80,10 @@ class ChronlyHMS {
       }
     }));
 
-    // input for milliseconds
-    // old
-
-    // document.querySelector("#sMS").addEventListener('change', function () {
-    //   if (!isNaN(this.value) && this.value.length === 2) {
-    //     this.value = '0' + this.value;
-    //   }
-
-    // });
 
     // milliseconds
     document.querySelectorAll("input.sMS").forEach(i => i.addEventListener('change', function () {
-    
+
       if (!isNaN(this.value) && this.value.length === 2) {
         this.value = '0' + this.value;
       }
@@ -116,59 +91,6 @@ class ChronlyHMS {
     }));
 
   }
-
-
-  // numeric value looping
-  // old
-
-  // addNumberLooping() {
-  //   // Numeric Value looping (eventually this can be 'parameterised' refactored)
-  //   // ex. 99 uparrow to 0 hours, 59 uparrow to 0 minutes and seconds, 990 uparrow to 010, downarrow 000 milliseconds
-
-
-  //   document.querySelector("#sH")
-  //     .addEventListener('change', function (event) {
-  //       if (document.querySelector("#sH").value == 100) {
-  //         document.querySelector("#sH").value = '00';
-  //       }
-  //       if (document.querySelector("#sH").value == -1) {
-  //         document.querySelector("#sH").value = 99;
-  //       }
-  //     });
-
-  //   document.querySelector("#sM")
-  //     .addEventListener('change', function (event) {
-  //       if (document.querySelector("#sM").value == 60) {
-  //         document.querySelector("#sM").value = '00';
-  //       }
-  //       if (document.querySelector("#sM").value == -1) {
-  //         document.querySelector("#sM").value = 59;
-  //       }
-  //     });
-
-  //   document.querySelector("#sS")
-  //     .addEventListener('change', function (event) {
-  //       if (document.querySelector("#sS").value == 60) {
-  //         document.querySelector("#sS").value = '00';
-  //       }
-  //       if (document.querySelector("#sS").value == -1) {
-  //         document.querySelector("#sS").value = 59;
-  //       }
-  //     });
-
-  //   document.querySelector("#sMS")
-  //     .addEventListener('change', function (event) {
-  //       if (document.querySelector("#sMS").value == 1000) {
-  //         document.querySelector("#sMS").value = "010";
-  //       }
-  //       if (document.querySelector("#sMS").value == -10) {
-  //         document.querySelector("#sMS").value = 990;
-  //       }
-  //       if (document.querySelector("#sMS").value == 0) {
-  //       }
-  //     });
-
-  // }
 
 
   addNumberLooping() {
@@ -232,14 +154,17 @@ class ChronlyHMS {
 
   // arrow key input of values (up/down) and place toggling (left/right)
 
-  addLeftRightToggle() {
+  addArrowKeyInput() {
 
     // Left and Right arrow key toggle between HMSmS
-    // this depends on data-attributes ex. data-tp="1" in the HTML
+    // this depends on data-attributes ex. data-tp="1" in HTML
 
-    document.querySelector("div.timeCase")
-      .addEventListener('keydown', function (event) {
+    document.querySelectorAll("div.timeCase").forEach(d =>
+      d.addEventListener('keydown', function (event) {
 
+
+        var et = event.target;
+        var dId = d.id;
         var tp_active;
 
         // these help manual testing by posting to the webapge
@@ -247,49 +172,57 @@ class ChronlyHMS {
         el_disp.innerHTML = "None";
 
         switch (true) {
+
           case event.key == "ArrowLeft":
             tp_active = document.activeElement.dataset.tp;
+
             if (tp_active >= 2) {
               tp_active--;
             }
-            document.querySelector("[data-tp='" + tp_active + "']").focus();
-            setTimeout(function () { document.querySelector("[data-tp='" + tp_active + "']").select(); }, 10);
+
+            document.querySelector('#'+ dId).querySelector("[data-tp='" + tp_active + "']").focus();
+            setTimeout(function () { document.querySelector('#'+ dId).querySelector("[data-tp='" + tp_active + "']").select(); }, 5);
             el_disp.innerHTML = event.key;
-            console.log("tp_active: " + tp_active);
+
             break;
 
           case event.key == "ArrowRight":
             tp_active = document.activeElement.dataset.tp;
+
             if (tp_active <= 3) {
               tp_active++;
             }
-            document.querySelector("[data-tp='" + tp_active + "']").focus();
-            setTimeout(function () { document.querySelector("[data-tp='" + tp_active + "']").select(); }, 10);
-            el_disp.innerHTML = event.key;
-            console.log("tp_active: " + tp_active)
-            break;
 
+            document.querySelector('#'+ dId).querySelector("[data-tp='" + tp_active + "']").focus();
+            setTimeout(function () { document.querySelector('#'+ dId).querySelector("[data-tp='" + tp_active + "']").select(); }, 5);
+            el_disp.innerHTML = event.key;
+
+            break;
 
           // these keep the input selected
           case event.key == "ArrowUp":
             el_disp.innerHTML = event.key;
             setTimeout(function () { document.activeElement.select(); }, 5);
+
             break;
 
           case event.key == "ArrowDown":
             el_disp.innerHTML = event.key;
+
             setTimeout(function () { document.activeElement.select(); }, 5);
+
             break;
 
           // this is good for testing, to determine whether a key press is detected
           case /^([0-9]?)$/.test(event.key):
+
             el_disp.innerHTML = event.key;
+
             break;
+
         }
-      });
+      }));
   }
-
-
 
   // numeric input (and auto toggling)
 
@@ -316,80 +249,6 @@ class ChronlyHMS {
         }
       });
     }
-
-    // three types of boxes: hours, minutes and seconds (are the same), or milliseconds
-    // old ID based
-
-    //   setInputFilter(document.getElementById("sH"), function (value) {
-
-
-    //     var newV = /^([0-9]?|[0-9][0-9]?)$/.test(value);
-
-    //     console.log("value: " + value);
-
-    //     if (value.length === 2) {
-    //       document.getElementById("sM").focus();
-    //       document.getElementById("sM").select();
-
-    //     }
-
-    //     return newV;
-
-    //   });
-
-
-    //   setInputFilter(document.getElementById("sM"), function (value) {
-
-
-    //     var newV = /^([0-9]?|[0-5][0-9]?)$/.test(value);
-
-    //     console.log("value: " + value);
-
-    //     if (value > 5 || value.length === 2) {
-    //       document.getElementById("sS").focus();
-    //       document.getElementById("sS").select();
-    //     }
-
-    //     return newV;
-
-    //   });
-
-
-    //   setInputFilter(document.getElementById("sS"), function (value) {
-
-
-    //     var newV = /^([0-9]?|[0-5][0-9]?)$/.test(value);
-
-    //     console.log("value: " + value);
-
-    //     if (value > 5 || value.length === 2) {
-    //       document.getElementById("sMS").focus();
-    //       document.getElementById("sMS").select();
-    //     }
-
-    //     return newV;
-
-    //   });
-
-
-
-    //   setInputFilter(document.getElementById("sMS"), function (value) {
-
-
-    //     var newV = /^([0-9]?|[0-9][0-9]?|[0-9][0-9][0-9]?)$/.test(value);
-
-    //     console.log("value: " + value);
-
-    //     if (value.length === 3) {
-    //       document.getElementById("sMS").blur();
-    //     }
-
-    //     return newV;
-
-    //   });
-
-    // }
-
 
 
     // three boxes, hours or minutes and seconds (are the same) but milliseconds is different
@@ -445,7 +304,7 @@ class ChronlyHMS {
     document.querySelectorAll('input.sMS').forEach(i => setInputFilter(i, function (value) {
 
       var newV = /^([0-9]?|[0-9][0-9]?|[0-9][0-9][0-9]?)$/.test(value);
-      
+
 
       if (value.length === 3) {
 
